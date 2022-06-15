b724b3157cbcea1964df20d5e9d7c9272cfb9dba
George Kibe <georgekibewambui@gmail.com>
2022-06-15 20:47:00 +0300

    finalizing post icons

--- src/components/Post/Footer.js
+++ src/components/Post/Footer.js
@@ -1,22 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Alert } from 'react-native'
+import React, {useState, useEffect} from 'react'
 import { StyleSheet } from 'react-native'
-import { Icon } from 'react-native-vector-icons/icon'
+import ADIcon from "react-native-vector-icons/AntDesign"
+import FontistoIcon from "react-native-vector-icons/Fontisto"
+import IonIcon from "react-native-vector-icons/Ionicons"
+import FontAwesome from "react-native-vector-icons/FontAwesome"
 
-const Footer = ({postedAt, caption, likesCount}) => {
+const Footer = ({postedAt, caption, likesCount:likesCountProp}) => {
+  const [isLiked, setIsLiked] = useState(false)
+  const [likesCount, setlikesCount] = useState(0)
+  const likePressed = () =>{
+    setIsLiked(!isLiked)
+    const amount = isLiked? -1 : 1 ;
+    setlikesCount(likesCount + amount)
+  }
+  useEffect(() => {
+    setlikesCount(likesCountProp)
+  }, [])
+  
   return (
     <View style={styles.container}>
+      <View style={styles.iconsContainer}>
+        <View style={styles.leftIcons}>
+          <TouchableOpacity onPress={likePressed}>
+            {
+              isLiked ? <ADIcon name="heart" size={25} color={"#dc143c"}/>
+              : <ADIcon name="hearto" size={25} color={"#545454"}/>
+            }
+            
+          </TouchableOpacity>
+          <FontistoIcon name="comment" size={23} color={"#545454"}/>
+          <IonIcon name="paper-plane-outline" size={28} color={"#545454"}/>
+        </View>
+        <FontAwesome name="bookmark-o" size={25}/>
+      </View>
       <Text style={styles.likes}>{likesCount} Likes</Text>
       <Text style={styles.caption}>{caption}</Text>
       <Text style={styles.postedAt}>{postedAt}</Text>
-      {/*<Icon name="rocket" size={30} color="#900"/> */}
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container:{
-
+    margin:10
+  },
+  iconsContainer:{
+    flexDirection:"row",
+    justifyContent:"space-between"
+  },
+  leftIcons:{
+    flexDirection:"row",
+    width:120,
+    justifyContent:"space-between"
   },
   likes:{
     fontWeight:'bold',
--- src/components/Post/Header.js
+++ src/components/Post/Header.js
@@ -2,12 +2,18 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import ProfilePicture from '../ProfilePicture'
 import { StyleSheet } from 'react-native'
+import Icon from "react-native-vector-icons/Entypo"
 
 const Header = ({imageUrl, name}) => {
   return (
     <View style={styles.container}>
-      <ProfilePicture uri={imageUrl} size={40}/>
-      <Text style={styles.name}>{name}</Text>
+      <View style={styles.left}>
+        <ProfilePicture uri={imageUrl} size={40}/>
+        <Text style={styles.name}>{name}</Text>
+      </View>
+      <View style={styles.right}>
+        <Icon name="dots-three-vertical" size={15}/>
+      </View>      
     </View>
   )
 }
@@ -15,7 +21,14 @@ const Header = ({imageUrl, name}) => {
 const styles = StyleSheet.create({
     container:{
         flexDirection:"row",
-        //alignItems:"center"
+        justifyContent:"space-between",
+        alignItems:"center"
+    },
+    left:{
+      flexDirection:"row"
+    },
+    right:{
+      marginRight:10,
     },
     name:{
         textAlign:"center",
