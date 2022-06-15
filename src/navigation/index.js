6c0b1a2bc10ce6494b2338544de617bb772f4fed
George Kibe <georgekibewambui@gmail.com>
2022-06-16 00:26:17 +0300

    finalizing navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,19 +1,65 @@
-import { View, Text } from 'react-native'
+import { View, Image } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeScreen from '../screens/HomeScreen'
+import Entypo from "react-native-vector-icons/Entypo"
+import FontAwesome from "react-native-vector-icons/FontAwesome"
+import MaterialIcons from "react-native-vector-icons/MaterialIcons"
+import Ionicons from "react-native-vector-icons/Ionicons"
+import HomeStackScreen from "./HomeStackScreen"
 
 const Tab = createBottomTabNavigator()
 
 const Navigator = () => {
   return (
-    <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Discovery" component={HomeScreen} />
-        <Tab.Screen name="Post" component={HomeScreen} />
-        <Tab.Screen name="Notifications" component={HomeScreen} />
-        <Tab.Screen name="Profile" component={HomeScreen} />
+    <Tab.Navigator screenOptions={{
+        "tabBarActiveTintColor": "#ff0000",
+        "tabBarInactiveTintColor": "#0000ff",
+        "tabBarShowLabel": true,
+        "tabBarStyle": [
+          {
+            "display": "flex"
+          },
+          null
+        ],
+        headerShown: false
+      }}>
+        <Tab.Screen name="Home" component={HomeStackScreen} 
+            options={{
+                tabBarIcon:({color}) => (
+                    <Entypo name="home" color={color} size={22} />
+                )
+            }}
+        />
+        <Tab.Screen name="Discovery" component={HomeScreen}
+            options={{
+                tabBarIcon:({color}) => (
+                    <FontAwesome name="search" color={color} size={22} />
+                )
+            }}
+        />
+        <Tab.Screen name="Post" component={HomeScreen} 
+             options={{
+                tabBarIcon:({color}) => (
+                    <MaterialIcons name="post-add" color={color} size={22} />
+                )
+            }}
+        />
+        <Tab.Screen name="Notifications" component={HomeScreen} 
+             options={{
+                tabBarIcon:({color}) => (
+                    <Ionicons name="notifications" color={color} size={22} />
+                )
+            }}
+        />
+        <Tab.Screen name="Profile" component={HomeScreen} 
+             options={{
+                tabBarIcon:({color}) => (
+                    <FontAwesome name="user-circle" color={color} size={22} />
+                )
+            }}
+        />
     </Tab.Navigator>
   )
 }
