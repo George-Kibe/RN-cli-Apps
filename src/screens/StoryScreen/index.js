e4e03f7a567beee5e071c4ad22d505e96ed4fb96
George Kibe <georgekibewambui@gmail.com>
2022-06-16 13:27:13 +0300

    stories view

--- src/screens/StoryScreen/index.js
+++ src/screens/StoryScreen/index.js
@@ -1,7 +1,10 @@
-import { SafeAreaView, ActivityIndicator, ImageBackground, TouchableWithoutFeedback,Dimensions} from 'react-native'
+import { Text, View, SafeAreaView, ActivityIndicator, ImageBackground, TouchableWithoutFeedback,Dimensions, Alert, TextInput} from 'react-native'
 import React, {useState, useEffect} from 'react'
-import { useRoute } from '@react-navigation/native'
+import { useRoute, useNavigation } from '@react-navigation/native'
 import stories from "../../data/stories"
+import ProfilePicture from "../../components/ProfilePicture"
+import Feather from "react-native-vector-icons/Feather"
+import IonIcons from "react-native-vector-icons/Ionicons"
 import styles from "./styles"
 
 const StoryScreen = () => {
@@ -9,10 +12,11 @@ const StoryScreen = () => {
   const [activeStoryIndex, setActiveStoryIndex] = useState(0)
   const [activeStory, setActiveStory] = useState(null)
 
+  const navigation = useNavigation()
   const route = useRoute()
+  const userId = route.params.userId
 
   useEffect(() => {
-    const userId = route.params.userId
     const userStory = stories.find(stories => stories.user.id === userId)
     setUserStory(userStory)
     setActiveStoryIndex(0)
@@ -33,10 +37,22 @@ const StoryScreen = () => {
 
 
   const handlePreviousStory = () =>{
+    if(activeStoryIndex === 0){
+      navigateToPreviousUser()
+    }
     setActiveStoryIndex(activeStoryIndex - 1)
   }
   const handleNextStory = () =>{
+    if(activeStoryIndex >= userStory.stories.length - 1){
+      navigateToNextUser()
+    }
     setActiveStoryIndex(activeStoryIndex + 1 )
+  }
+  const navigateToNextUser = () =>{
+    navigation.push("Story", {userId: (parseInt(userId) + 1).toString() })
+  }
+  const navigateToPreviousUser = () =>{
+    navigation.push("Story", {userId: (parseInt(userId) - 1).toString() })
   }
 
   const handlePress = (event) =>{
@@ -64,6 +80,19 @@ const StoryScreen = () => {
     <SafeAreaView style={styles.container}>      
       <TouchableWithoutFeedback onPress={handlePress}>
         <ImageBackground source={{uri:activeStory.imageUrl}} style={styles.image}>
+          <View style={styles.userInfo}>
+            <ProfilePicture uri={userStory.user.imageUrl} size={50}/>
+            <Text style={styles.username}>{userStory.user.name}</Text>
+            <Text style={styles.postedTime}>{activeStory.postedTime}</Text>
+          </View>
+          <View style={styles.bottomContainer}>
+            <View style={styles.cameraAndInput}>
+              <Feather style={styles.cameraButton} name="camera" size={35} color={"#ffffff"}/>
+              <TextInput style={styles.textInput} placeholderTextColor={"white"}
+                placeholder='Send a message...' />  
+            </View>             
+            <IonIcons style={styles.messageButton} name="paper-plane-outline" size={35} color={"#ffffff"}/>             
+          </View>          
         </ImageBackground>
       </TouchableWithoutFeedback>
     </SafeAreaView>
